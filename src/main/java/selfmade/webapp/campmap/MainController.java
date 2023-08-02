5676c4d820642a890bd81dec500ece899c443238
Fukui <fukujavai@gmail.com>
2023-08-02 13:39:10 +0900

    2回目

--- src/main/java/selfmade/webapp/campmap/MainController.java
+++ src/main/java/selfmade/webapp/campmap/MainController.java
@@ -12,44 +12,77 @@ import java.awt.datatransfer.DataFlavor;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
 @Controller
 public class MainController {
     record CampItem(String id, int number, String name, String ken, String city, String detail) {
     }
-//    record KenItem(int value){}
+
+    record KenItem(int value) {
+    }
 
     private List<CampItem> campItems = new ArrayList<>();
-//    private List<KenItem> kenItems = new ArrayList<>();
+    private ArrayList<KenItem> kenItems = new ArrayList<>();
 
     @GetMapping("/list")
     String listItems(Model model) {
         model.addAttribute("campList", campItems);
         return "main";
     }
-//    @RequestParam()
-//    String kenItems(Model model) {
-//        model.addAttribute("kenList", kenItems);
-//        return "/add";
-//    }
 
     @GetMapping("/add")
-        String addItem (){
+    String kenItems() {
         String id = UUID.randomUUID().toString().substring(0, 8);
-        switch (1) {
-            case 1:
-                int number = 1;
-                String name = "キャンプ場名";
-                String ken = "北海道";
-                String city  = "札幌市";
-                String detail = "詳細へ";
-                CampItem item = new CampItem(id, number, name, ken, city, detail);
-                campItems.add(item);
-                break;
-        }
+        int number = 1;
+        String name = "aaa";
+        String ken = "北海道";
+        String city = "札幌市";
+        String detail = "詳細へ";
+
+        CampItem item = new CampItem(id, number, name, ken, city, detail);
+        campItems.add(item);
+
+
+        return "redirect:/list";
+    }
+
+//    @GetMapping("/add2")
+//        String addItem(){
+//
+//        String id = UUID.randomUUID().toString().substring(0, 8);
+//        int number = 1;
+//        String name = "aaa";
+//        String ken = "北海道";
+//        String city = "札幌市";
+//        String detail = "詳細へ";
+//
+//        CampItem item = new CampItem(id,number, name, ken, city, detail);
+//        campItems.add(item);
+//
+//
+//        return "redirect:/list";
+//    }
+
+    @GetMapping("/2")
+    String addItem2 (){
+        String id = UUID.randomUUID().toString().substring(0, 8);
+        int number = 1;
+        String name = "aaa";
+        String ken = "aaa";
+        String city = "札幌市";
+        String detail = "詳細へ";
+
+        CampItem item = new CampItem(id,number, name, ken, city, detail);
+        campItems.add(item);
+
+
         return "redirect:/list";
     }
 }
+
 //    @GetMapping("/add")
 //    String addItem() {
 //        String id = UUID.randomUUID().toString().substring(0, 8);
