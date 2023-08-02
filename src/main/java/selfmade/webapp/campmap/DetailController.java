159becfa20afdbe6de694d663a7b5b49596936fb
Fukui <fukujavai@gmail.com>
2023-08-02 14:12:40 +0900

    pullの説明を追加

---
+++ src/main/java/selfmade/webapp/campmap/DetailController.java
@@ -0,0 +1,17 @@
+package selfmade.webapp.campmap;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/campmap")
+public class DetailController {
+    @GetMapping("detail")
+    public String init(Model model) {
+        model.addAttribute();
+
+        return "each";
+    }
+}
