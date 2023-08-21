1f6217b0a75e8753b31b99cfac38fd8219cdfff0
Fukui <fukujavai@gmail.com>
2023-08-21 14:00:53 +0900

    mysqlと連携させた。

--- src/main/java/selfmade/webapp/campmap/DetailController.java
+++ src/main/java/selfmade/webapp/campmap/DetailController.java
@@ -1,17 +1,21 @@
 package selfmade.webapp.campmap;
 
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//@Controller
-//@RequestMapping("/campmap")
-//public class DetailController {
-//    @GetMapping("detail")
-//    public String init(Model model) {
-//        model.addAttribute();
-//
-//        return "each";
-//    }
-//}
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/campmap")
+public class DetailController {
+    record DetailItem(String id, String name, String address){}
+    private List<DetailItem> detailItems = new ArrayList<>();
+    @GetMapping("/detail/")
+    public String init(Model model) {
+        model.addAttribute("detailList", detailItems);
+        return "detail";
+    }
+}
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS camplist (
+  id varchar(8) primary key,
+  number varchar(8),
+  name varchar(16),
+  address varchar(16),
+  detail varchar(255)
+);
